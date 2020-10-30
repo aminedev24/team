1d1941cad04507c4a90224311e634ad36e134b1e
aminedz24 <54458000+aminedz24@users.noreply.github.com>
2020-10-30 13:18:57 +0100

    Create mobile.js

---
+++ js/mobile.js
@@ -0,0 +1,19 @@
+var query = window.matchMedia('(max-width: 550px)');
+
+if(query.matches){
+    document.querySelector(".search-icon").addEventListener("click", function(){
+        document.querySelector(".search").classList.toggle("active");
+        switch(counter){
+            case 1:
+                document.querySelector('.search').style.visibility = 'visible';
+                document.querySelector('.search').style.width = '100%';
+                break;
+            default:
+                document.querySelector('.search').style.visibility = '';
+                document.querySelector('.search').style.width = '';
+                counter = 0;
+        }
+        counter++;
+    });
+    
+}
