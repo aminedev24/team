eb9608e2ab22e0c9a748214ba5101bca58a633fa
aminedz24 <54458000+aminedz24@users.noreply.github.com>
2020-10-30 13:18:03 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 
+var button = document.getElementById('button');
+
+var center = document.getElementById('center');
+
+var container = document.getElementById('container');
+
+
+
+var colorChange = document.getElementById('buttonc');
+
+
+var counter = 1;
+
+colorChange.onclick = function () {
+  //'use strict';
+  //counter ++;
+  switch (counter){
+    case 1:
+      container.style.backgroundColor = 'green';
+    break;
+    case 2:
+     container.style.backgroundColor = 'grey';
+    break;
+    case 3:
+      container.style.backgroundColor = 'steelblue';
+      
+    break;
+    default:
+      counter = 0;
+      //Block of code 
+      container.style.backgroundColor = 'cadetblue'; 
+  }
+  counter++;
+}
+
